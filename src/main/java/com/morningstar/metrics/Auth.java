0bf686e5d9de4465a7b43c5543e1a305d10362ae
Sameen Islam <ec14370@qmul.ac.uk>
2016-05-17 00:16:05 +0100

    Created package and class for auth

---
+++ src/main/java/com/morningstar/metrics/Auth.java
@@ -0,0 +1,16 @@
+package main.java.com.morningstar.metrics;
+
+/**
+ * Created by sameenislam on 16/05/2016.
+ * Authenticates with the Waka Time API using basic authentication.
+ */
+public class Auth {
+
+    private String resourceUrl = "https://wakatime.com/api/v1/users/current/durations?date=2016-05-15"
+    private String encodedAPISecret = "ZmUxY2E5ZDQtNTBjZi00MjA1LTk4NGUtMmEwZTQxZWFhMWI3";
+    private String AUTH_TYPE = "Basic";
+    private String apiReqKey = AUTH_TYPE + encodedAPISecret;
+
+
+
+}
