1e445e359213c57b3e3f46ecee0e78d037ce9062
Sameen Islam <ec14370@qmul.ac.uk>
2016-05-30 20:16:07 +0100

    Added view controller for clock.

---
+++ src/main/java/timer/StarClockController.java
@@ -0,0 +1,31 @@
+package main.java.timer;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by sameenislam on 30/05/2016.
+ */
+public class StarClockController implements Initializable{
+    @FXML private Label txtTimer;
+    private String INITIAL_TIMER_VALUE = "00:00";
+
+    // pop up for add new task button
+    public static void onClickAddTaskBtn() throws Exception{
+        throw new Exception("Feature not yet implemented!");
+    }
+
+    // play and pause button controller
+    public static void onClickPpBtn() throws Exception{
+        throw new Exception("Feature not yet implemented!");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        txtTimer.setText(INITIAL_TIMER_VALUE);
+    }
+}
---
+++ src/test/java/TimerTest.java
@@ -0,0 +1,11 @@
+package test.java;
+
+/**
+ * Created by sameenislam on 30/05/2016.
+ */
+public class TimerTest {
+
+    public static void main(String[] args) {
+
+    }
+}
